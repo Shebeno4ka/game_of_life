4f71aaaf620901cf8c8f8d8dc093df3e923eab1a
Shebeno4ka <shebeno4ka@outlook.com>
2025-05-27 10:10:57 +0300

    docs: game_renderer

--- src/game_renderer.hpp
+++ src/game_renderer.hpp
@@ -16,6 +16,10 @@ constexpr SDL_Color kBackgroundColor = {0, 0, 0, 255};
 constexpr SDL_Color kCellColor = {255, 255, 255, 255};
 constexpr SDL_Color kHoveredCellColor = {255, 0, 0, 128};
 
+/**
+ * \class GameRenderer
+ * \brief Handles rendering of the game field and user interactions.
+ */
 class GameRenderer {
   std::shared_ptr<ThreadSafeGameState> state_thread_guard_ptr_;
   std::shared_ptr<SDL_Renderer> renderer_ptr_;
@@ -23,16 +27,44 @@ class GameRenderer {
   std::optional<std::pair<uint32_t, uint32_t>> hovered_cell_coords_;
 
  public:
+  /**
+   * \brief Constructs a GameRenderer instance.
+   * \param state_ptr Shared pointer to thread-safe game state.
+   * \param renderer_ptr Shared pointer to SDL renderer.
+   * \param game_runner Unique pointer to started game runner instance.
+   */
   GameRenderer(std::shared_ptr<ThreadSafeGameState> state_ptr,
                std::shared_ptr<SDL_Renderer> renderer_ptr,
-               GameRunner& game_runner);
+               std::unique_ptr<GameRunner> game_runner);
 
+  /**
+   * \brief Starts the rendering process.
+   */
   void start();
 
  private:
+  /**
+   * \brief Handles user input events and updates the game state.
+   * \param size_x Width of the game field in cells.
+   * \param size_y Height of the game field in cells.
+   * \param running Reference to the running state of the game loop.
+   * \param paused Reference to the paused state of the game loop.
+   */
   void handleEvents_(uint64_t size_x, uint64_t size_y, bool& running,
                      bool& paused);
+
+  /**
+   * \brief Renders the game field and other visual elements.
+   */
   void render_();
+
+  /**
+   * \brief Draws the game field based on the current game state.
+   */
   void drawField_();
+
+  /**
+   * \brief Highlights the currently hovered cell.
+   */
   void drawHoveredCell_();
 };
