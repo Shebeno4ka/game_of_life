cb6a3e42795d23503da918ece3976c7354303dc3
sshheebbee <direktor.arbuznogo.zavoda@gmail.com>
2025-06-11 21:44:14 +0300

    int

--- src/game_event.hpp
+++ src/game_event.hpp
@@ -1,34 +1,19 @@
 /*
-* std::visit(EventVisitor{}, event);
-*/
+ * std::visit(EventVisitor{}, event);
+ */
 
 #pragma once
 
 #include <cstdint>
 #include <variant>
 
-
 struct AddCellEvent {
-  uint32 x;
-  iunt32 y;
+  uint32_t x;
+  uint32_t y;
 };
 
 struct PauseEvent {};
 
 struct UnPauseEvent {};
-
-struct EventVisitor {
-  void operator()(const AddCellEvent& e) {
-    throw UnimplementedException("Unimplemented1");
-  }
-
-  void operator()(const PauseEvent& e) {
-    throw UnimplementedException("Unimplemented2");
-  }
-
-  void operator()(const UnPauseEvent& e) {
-    throw UnimplementedException("Unimplemented3");
-  }
-};
 
 using GameEvent = std::variant<AddCellEvent, PauseEvent, UnPauseEvent>;
