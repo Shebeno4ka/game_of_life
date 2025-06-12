d95a70c377498f1ce48eb78aa1fec1fa53ff49fb
sshheebbee <direktor.arbuznogo.zavoda@gmail.com>
2025-06-13 00:02:28 +0300

    feat: add frontend

--- src/main.cpp
+++ src/main.cpp
@@ -10,66 +10,71 @@
 #include "game_state.hpp"
 #include "thread_safe_game_state.hpp"
 
-using sdl_window_ptr = std::shared_ptr<SDL_Window>;
+#include "ws_controller.hpp"
 
-int startGame(Options& opts) {
-  if (!SDL_Init(SDL_INIT_VIDEO)) {
-    std::cerr << "SDL init failed: " << SDL_GetError() << std::endl;
-    return 1;
-  }
+// using sdl_window_ptr = std::shared_ptr<SDL_Window>;
 
-  auto sdl_window = SDL_CreateWindow("Game of Life", opts.size_x * kCellPixels,
-                                     opts.size_y * kCellPixels,
-                                     SDL_WINDOW_HIGH_PIXEL_DENSITY);
-  if (!sdl_window) {
-    std::cerr << "Window creation error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
-  std::shared_ptr<SDL_Window> window_ptr(sdl_window, &SDL_DestroyWindow);
+// int startGame(Options& opts) {
+//   if (!SDL_Init(SDL_INIT_VIDEO)) {
+//     std::cerr << "SDL init failed: " << SDL_GetError() << std::endl;
+//     return 1;
+//   }
 
-  SDL_Renderer* renderer = SDL_CreateRenderer(window_ptr.get(), nullptr);
-  if (!renderer) {
-    std::cerr << "Renderer creation error: " << SDL_GetError() << std::endl;
-    return 1;
-  }
-  std::shared_ptr<SDL_Renderer> renderer_ptr(renderer, &SDL_DestroyRenderer);
+//   auto sdl_window = SDL_CreateWindow("Game of Life", opts.size_x *
+//   kCellPixels,
+//                                      opts.size_y * kCellPixels,
+//                                      SDL_WINDOW_HIGH_PIXEL_DENSITY);
+//   if (!sdl_window) {
+//     std::cerr << "Window creation error: " << SDL_GetError() << std::endl;
+//     return 1;
+//   }
+//   std::shared_ptr<SDL_Window> window_ptr(sdl_window, &SDL_DestroyWindow);
 
-  SDL_SetRenderVSync(renderer_ptr.get(), 1);
+//   SDL_Renderer* renderer = SDL_CreateRenderer(window_ptr.get(), nullptr);
+//   if (!renderer) {
+//     std::cerr << "Renderer creation error: " << SDL_GetError() << std::endl;
+//     return 1;
+//   }
+//   std::shared_ptr<SDL_Renderer> renderer_ptr(renderer, &SDL_DestroyRenderer);
 
-  std::vector field(static_cast<size_t>(opts.size_y),
-                    std::vector(static_cast<size_t>(opts.size_x), false));
+//   SDL_SetRenderVSync(renderer_ptr.get(), 1);
 
-  GameState&& game_state = GameState(std::move(field));
+//   std::vector field(static_cast<size_t>(opts.size_y),
+//                     std::vector(static_cast<size_t>(opts.size_x), false));
 
-  auto game_runner = std::make_unique<GameRunner>(std::move(game_state),
-                                                  opts.updates_per_second);
-  game_runner->start();
+//   GameState&& game_state = GameState(std::move(field));
 
-  // GameSDLRenderer game_renderer(game_state, renderer_ptr,
-  //                               std::move(game_runner));
-  // game_renderer.start();
+//   auto game_runner = std::make_unique<GameRunner>(std::move(game_state),
+//                                                   opts.updates_per_second);
+//   game_runner->start();
 
-  SDL_Quit();
-  return 0;
-}
+//   // GameSDLRenderer game_renderer(game_state, renderer_ptr,
+//   //                               std::move(game_runner));
+//   // game_renderer.start();
+
+//   SDL_Quit();
+//   return 0;
+// }
 
 int main(int argc, char* argv[]) {
-  Options opts_t{50, 100, 10};
-  return startGame(opts_t);
-  std::optional<Options> opts;
+  WSServer ws_server;
+  ws_server.listenToConnections();
+  // Options opts_t{50, 100, 10};
+  // return startGame(opts_t);
+  // std::optional<Options> opts;
 
-  try {
-    opts = ParseCommandLineOptions(argc, argv);
-  } catch (const invalid_argument_exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    std::cerr << "Type \"GameOfLife -h\" for help" << std::endl;
-    return 1;
-  }
+  // try {
+  //   opts = ParseCommandLineOptions(argc, argv);
+  // } catch (const invalid_argument_exception& e) {
+  //   std::cerr << "Error: " << e.what() << std::endl;
+  //   std::cerr << "Type \"GameOfLife -h\" for help" << std::endl;
+  //   return 1;
+  // }
 
-  if (!opts) {
-    std::cout << kHelpText;
-    return 0;
-  }
+  // if (!opts) {
+  //   std::cout << kHelpText;
+  //   return 0;
+  // }
 
-  return startGame(*opts);
+  // return startGame(*opts);
 }
---
+++ src/ws_controller.hpp
@@ -0,0 +1,163 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <variant>
+#include <vector>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+
+#include "game_event.hpp"
+
+namespace beast = boost::beast;          // from <boost/beast.hpp>
+namespace http = beast::http;            // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket;  // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;             // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;        // from <boost/asio/ip/tcp.hpp>
+
+class WSServer {
+  std::vector<std::shared_ptr<websocket::stream<tcp::socket>>> clients_;
+
+ public:
+  WSServer() = default;
+  ~WSServer() = default;
+
+ private:
+  // Parse incoming message to GameEvent
+  std::optional<GameEvent> parseMessage(const std::string& message) {
+    if (message == "pause") {
+      return PauseEvent{};
+    } else if (message == "unpause") {
+      return UnPauseEvent{};
+    } else {
+      // Try to parse coordinates "x,y"
+      std::istringstream iss(message);
+      std::string x_str, y_str;
+
+      if (std::getline(iss, x_str, ',') && std::getline(iss, y_str)) {
+        try {
+          uint32_t x = std::stoul(x_str);
+          uint32_t y = std::stoul(y_str);
+          return AddCellEvent{x, y};
+        } catch (const std::exception&) {
+          // Invalid format
+          return std::nullopt;
+        }
+      }
+    }
+
+    return std::nullopt;
+  }
+
+  // Process GameEvent
+  void processGameEvent(const GameEvent& event) {
+    std::visit(
+        [](const auto& e) {
+          using T = std::decay_t<decltype(e)>;
+          if constexpr (std::is_same_v<T, AddCellEvent>) {
+            std::cout << "AddCell event: x=" << e.x << ", y=" << e.y
+                      << std::endl;
+            // TODO: Add cell to game state
+          } else if constexpr (std::is_same_v<T, PauseEvent>) {
+            std::cout << "Pause event received" << std::endl;
+            // TODO: Pause game
+          } else if constexpr (std::is_same_v<T, UnPauseEvent>) {
+            std::cout << "Unpause event received" << std::endl;
+            // TODO: Unpause game
+          }
+        },
+        event);
+  }
+
+ public:
+  void listenToConnections() {
+    try {
+      // Use IP address instead of hostname to avoid resolution issues
+      auto const address = net::ip::make_address("127.0.0.1");
+      auto const port = static_cast<unsigned short>(8080);
+
+      // The io_context is required for all I/O
+      net::io_context ioc{1};
+
+      // The acceptor receives incoming connections
+      tcp::acceptor acceptor{ioc, {address, port}};
+
+      // Set socket options to allow reuse of address
+      acceptor.set_option(net::socket_base::reuse_address(true));
+
+      std::cout << "WebSocket server listening on " << address << ":" << port
+                << std::endl;
+
+      for (;;) {
+        tcp::socket socket{ioc};
+        acceptor.accept(socket);
+        std::thread(&WSServer::do_session, this, std::move(socket)).detach();
+      }
+    } catch (const std::exception& e) {
+      std::cerr << "Error: " << e.what() << std::endl;
+    }
+  }
+
+  // Echoes back all received WebSocket messages
+  void do_session(tcp::socket socket) {
+    try {
+      // Construct the stream by moving in the socket
+      websocket::stream<tcp::socket> ws{std::move(socket)};
+
+      // Set a decorator to change the Server of the handshake
+      ws.set_option(
+          websocket::stream_base::decorator([](websocket::response_type& res) {
+            res.set(http::field::server,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-server-sync");
+          }));
+
+      // Accept the websocket handshake
+      ws.accept();
+
+      for (;;) {
+        // This buffer will hold the incoming message
+        beast::flat_buffer buffer;
+
+        // Read a message
+        ws.read(buffer);
+
+        // Convert buffer to string
+        std::string message = beast::buffers_to_string(buffer.data());
+
+        // Parse message to GameEvent
+        auto event = parseMessage(message);
+        if (event.has_value()) {
+          // Process the event
+          processGameEvent(event.value());
+
+          // Send acknowledgment or game state update
+          std::string response = "Event processed: " + message;
+          ws.text(true);
+          ws.write(net::buffer(response));
+        } else {
+          // Invalid message format
+          std::string error = "Invalid message format: " + message;
+          ws.text(true);
+          ws.write(net::buffer(error));
+        }
+      }
+    } catch (beast::system_error const& se) {
+      // This indicates that the session was closed
+      if (se.code() != websocket::error::closed)
+        std::cerr << "Error: " << se.code().message() << std::endl;
+    } catch (std::exception const& e) {
+      std::cerr << "Error: " << e.what() << std::endl;
+    }
+  }
+
+  void onMessage(const std::string& message);
+};
