f7da773cc3bd9d151957179f3fcea5be96a2a05d
sshheebbee <direktor.arbuznogo.zavoda@gmail.com>
2025-06-14 17:24:59 +0300

    feat: change main to listen inside docker

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include "game_runner.hpp"
 #include "ws_controller.hpp"
 
 int main(int argc, char* argv[]) {
-  WSServer ws_server;
-  ws_server.startListening("0.0.0.0", 8080);
+  auto game_runner = std::make_shared<GameRunner>(GameState(50, 40), 8);
+  WSServer ws_server(game_runner);
+  ws_server.startListening("127.0.0.1", 8080);
 }
