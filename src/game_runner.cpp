b7c6f628a925893d5446b1b613b1bd78ced5db35
sshheebbee <direktor.arbuznogo.zavoda@gmail.com>
2025-06-14 17:23:05 +0300

    feat: rewrite game_runner for events

--- src/game_runner.cpp
+++ src/game_runner.cpp
@@ -1,13 +1,12 @@
 #include "game_runner.hpp"
 
-#include <utility>
-#include "game_state.hpp"
-#include "thread_safe_game_state.hpp"
-
-GameRunner::GameRunner(GameState&& state, const uint32_t updates_per_second)
-    : event_queue_(EVENT_QUEUE_SIZE),
+GameRunner::GameRunner(GameState&& state,
+                       const uint32_t updates_per_second,
+                       const uint32_t updates_send_per_second)
+    : filtered_event_queue_(EVENT_QUEUE_SIZE),
       state_(std::move(state)),
       updates_per_second_(updates_per_second),
+      updates_send_per_second_(updates_send_per_second),
       running_(false),
       paused_(true) {}
 
@@ -18,6 +17,7 @@ GameRunner::~GameRunner() {
 void GameRunner::start() {
   running_ = true;
   game_thread_ = std::thread(&GameRunner::updateLoop_, this);
+  event_handler_thread_ = std::thread(&GameRunner::handleEventsLoop_, this);
 }
 
 void GameRunner::stop() {
@@ -29,20 +29,63 @@ void GameRunner::stop() {
 }
 
 void GameRunner::addEvent(GameEvent&& event) {
-  event_queue_.push(event);
+  filtered_event_queue_.push(event);
 }
 
-void GameRunner::pause() {
-  paused_.store(true);
+void GameRunner::handleEventsLoop_() {
+  while (running_.load()) {
+    GameEvent event;
+    filtered_event_queue_.consume_one(
+        [this](GameEvent&& event) { processEvent_(std::move(event)); });
+    for (auto& callback : field_update_callbacks_) {
+      auto field_guard = state_.getStateGuard();
+      callback(field_guard.get().getField());
+    }
+  }
 }
 
-void GameRunner::resume() {
-  paused_.store(false);
-  pause_cond_var_.notify_one();
+void GameRunner::processEvent_(GameEvent&& event) {
+  std::visit(
+      [this](const auto& e) {
+        using T = std::decay_t<decltype(e)>;
+        if constexpr (std::is_same_v<T, AddCellEvent>) {
+          auto field_guard = state_.getStateGuard();
+          field_guard.get().toggleCell(e.x, e.y);
+        } else if constexpr (std::is_same_v<T, PauseEvent>) {
+          if (!running_.load())
+            return;  // Ignore pause if not running
+          paused_.store(true);
+        } else if constexpr (std::is_same_v<T, UnPauseEvent>) {
+          if (!running_.load())
+            return;  // Ignore unpause if not running
+          paused_.store(false);
+          pause_cond_var_.notify_all();
+        }
+      },
+      event);
 }
 
 void GameRunner::updateLoop_() {
   auto last_time = std::chrono::steady_clock::now();
+  auto do_update = [&, this] {
+    // Getting access to the game state
+    auto field_guard = state_.getStateGuard();
+    auto& state = field_guard.get();
+
+    // Calculating how much updates we need to do since the last update
+    auto current_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_time = current_time - last_time;
+    double delta_time = elapsed_time.count();
+    if (delta_time >= 1.0 / updates_per_second_) {
+      // Perform updates until we catch up with the elapsed time
+      while (delta_time >= 1.0 / updates_per_second_) {
+        state.update();
+        delta_time -= 1.0 / updates_per_second_;
+      }
+      last_time = current_time;
+    }
+  };
+
   while (running_.load()) {
     if (paused_.load()) {
       std::unique_lock lock(pause_mutex_);
@@ -53,22 +96,6 @@ void GameRunner::updateLoop_() {
       last_time = std::chrono::steady_clock::now();
     }
 
-    {
-      // Getting access to the game state
-      auto field_guard = state_.getStateGuard();
-      auto& state = field_guard.get();
-
-      // Calculating how much updates we need to do since the last update
-      auto current_time = std::chrono::steady_clock::now();
-      std::chrono::duration<double> elapsed_time = current_time - last_time;
-      double delta_time = elapsed_time.count();
-      if (delta_time >= 1.0 / updates_per_second_) {
-        while (delta_time >= 1.0 / updates_per_second_) {
-          state.update();
-          delta_time -= 1.0 / updates_per_second_;
-        }
-        last_time = current_time;
-      }
-    }
+    do_update();
   }
 }
--- src/game_runner.hpp
+++ src/game_runner.hpp
@@ -7,6 +7,7 @@
 
 #include <boost/lockfree/queue.hpp>
 
+#include <utility>
 #include "game_event.hpp"
 #include "game_state.hpp"
 #include "thread_safe_game_state.hpp"
@@ -31,14 +32,21 @@ constexpr size_t EVENT_QUEUE_SIZE = 100;
  * thread-safe access to the game state.
  */
 class GameRunner {
+  using FieldUpdateCallback =
+      std::function<void(std::vector<std::vector<bool>>)>;
+
+  lockfree_queue filtered_event_queue_;
   lockfree_queue event_queue_;
   const uint32_t updates_per_second_;
   std::atomic_bool running_;
   std::atomic_bool paused_;
+  std::atomic_int32_t updates_send_per_second_;
   std::thread game_thread_;
+  std::thread event_handler_thread_;
   mutable std::mutex pause_mutex_;
   mutable std::condition_variable pause_cond_var_;
   ThreadSafeGameState state_;
+  std::vector<FieldUpdateCallback> field_update_callbacks_;
 
  public:
   /**
@@ -46,7 +54,9 @@ class GameRunner {
    * \param state Shared pointer to the thread-safe game state.
    * \param updates_per_second Number of updates per second for the game loop.
    */
-  GameRunner(GameState&& state, uint32_t updates_per_second);
+  GameRunner(GameState&& state,
+             const uint32_t updates_per_second = 6,
+             const uint32_t updates_send_per_second = 100);
   ~GameRunner();
 
   /**
@@ -59,26 +69,42 @@ class GameRunner {
    */
   void stop();
 
+  /**
+   * \brief Adds an event to the event queue.
+   */
   void addEvent(GameEvent&& event);
 
-  void pause();
-
-  void resume();
+  /**
+   * \brief Registers a callback to be called when game field changes.
+   * \param callback The callback function to register.
+   * The callback should accept a vector of booleans representing the field.
+   * This callback will be called whenever the game field is updated.
+   */
+  void registerFieldUpdateCallback(FieldUpdateCallback callback);
 
  private:
   /**
-   * \brief The main update loop for the game.
+   * \brief Update loop for the game.
    *
    * This function runs in a separate thread and updates the game state
    * at the specified rate.
    */
   void updateLoop_();
 
-  struct EventVisitor {
-    void operator()(const AddCellEvent& e);
+  /**
+   * \brief Handles events in a separate thread.
+   *
+   * This function processes events from the event queue and updates
+   * the game state accordingly.
+   */
+  void handleEventsLoop_();
 
-    void operator()(const PauseEvent& e);
-
-    void operator()(const UnPauseEvent& e);
-  };
+  /**
+   * \brief Processes a game event.
+   *
+   * This function is called by the event handler thread to process
+   * events from the event queue.
+   * \param event The game event to process.
+   */
+  void processEvent_(GameEvent&& event);
 };
