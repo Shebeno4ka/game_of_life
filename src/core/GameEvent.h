e6d67ead212feee7d22bbe82783e976b9994a9fe
sshheebbee <direktor.arbuznogo.zavoda@gmail.com>
2025-06-14 19:03:25 +0300

    chore: move class to another file

---
+++ src/core/GameEvent.h
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <vector>
+
+#include "GameSimulator.h"
+
+struct CellChange
+{
+    uint32_t x, y;
+    bool alive;
+};
+
+/**
+ * Событие от клиента - изменение набора клеток
+ */
+struct GameEvent
+{
+    std::vector<CellChange> changes;
+
+    GameEvent(std::vector<CellChange> cellChanges) : changes(std::move(cellChanges))
+    {
+    }
+
+    GameEvent() = default;
+
+    GameEvent(GameEvent &&) = default;
+    GameEvent &operator=(GameEvent &&) = default;
+
+    void Run(LifeGame::GameSimulator &simulator)
+    {
+        for (const auto &[x, y, alive] : changes)
+        {
+            simulator.applySingleCellChange(x, y, alive);
+        }
+    }
+};
